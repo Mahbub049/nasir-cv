8cbb1d37e4371720274839ca49692906c6c0e564
Mahbub049 <mahbubsarwar49@gmail.com>
2025-07-05 02:21:41 +0600

    link update

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://nasir-cv-server.onrender.com/api/api", // https://nasir-cv-server.onrender.com/api
+  baseURL: "https://nasir-cv-server.onrender.com/api", // https://nasir-cv-server.onrender.com/api
 });
 
 export default instance;
